49e85b1c74fe695982a14590bbbed175451cecc7
Cleber Lucas Farias <clfm@cin.ufpe.br>
2024-07-27 21:57:52 -0300

    Implementing ADDI instruction

--- design/ALUController.sv
+++ design/ALUController.sv
@@ -15,37 +15,32 @@ module ALUController (
 );
 
   assign Operation[0] =
-    // R\I- or
+    // R\I - OR 
     ((ALUOp == 2'b10) && (Funct3 == 3'b110) && (Funct7 == 7'b0000000)) ||     
-    // R\I- srli  
-    ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000)) ||      
-    // R\I- srai - Não Implementda
-    ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||
-    // R\I- sub
-    ((ALUOp == 2'b10) && (Funct3 == 3'b000) && (Funct7 == 7'b0100000));             
+    // R\I - SUB
+    ((ALUOp == 2'b10) && (Funct3 == 3'b000) && (Funct7 == 7'b0100000));            
+
+////////////////////////////////////////////////////////////////////////////////////////////////
 
   assign Operation[1] = 
     // LW\SW
     (ALUOp == 2'b00) ||  
-    // R\I-add
-    ((ALUOp == 2'b10) && (Funct3 == 3'b000)) ||  
-    // R\I->>>
-    ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||
-    // R\I- sub
+    // R\I - ADD
+    ((ALUOp == 2'b10) && (Funct3 == 3'b000))||  
+    // R\I - SUB
     ((ALUOp == 2'b10) && (Funct3 == 3'b000) && (Funct7 == 7'b0100000)); 
+
+////////////////////////////////////////////////////////////////////////////////////////////////
 
   assign Operation[2] =  
     // R\I - Xor
-    ((ALUOp == 2'b10) && (Funct3 == 3'b100) && (Funct7 == 7'b0000000)) ||
+    ((ALUOp == 2'b10) && (Funct3 == 3'b100) && (Funct7 == 7'b0000000));
     
-    ((ALUOp==2'b10) && (Funct3==3'b101) && (Funct7==7'b0000000)) || // R\I->>
-    ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||  // R\I->>>
-    ((ALUOp == 2'b10) && (Funct3 == 3'b001)) ||  // R\I-<<
-    ((ALUOp == 2'b10) && (Funct3 == 3'b010));  // R\I-<
+////////////////////////////////////////////////////////////////////////////////////////////////
 
   assign Operation[3] = 
-      (ALUOp == 2'b01) ||  // BEQ
-      ((ALUOp == 2'b10) && (Funct3 == 3'b010)) ||       // R\I-<
-      // R\I- srli 
-      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000));
+    // BEQ
+    (ALUOp == 2'b01) || 
+    // R\I- srli 
+    ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000));
 endmodule
